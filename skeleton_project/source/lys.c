6036d3ad7f1f748549cfaa59e0209817abfad74a
smigur <113334985+smigur@users.noreply.github.com>
2026-02-23 09:23:27 +0100

    deklarerer funksjoner

--- skeleton_project/source/lys.c
+++ skeleton_project/source/lys.c
@@ -1,2 +1,16 @@
 #include "lys.h"
 #include <stdio.h>
+
+// antar at å sette et element i lyslisten til true også aktiverer lyset på boksen
+/*
+REDUNTANT:
+aktiverLys(Enum lysType, int lysIndex, bool On):
+	knappLys[lysType][lysIndex] = On
+*/
+
+aktiverEtasjeLys(int lysIndex):
+	# clear lysene som er aktive:
+	for (int i = 0; i < 4; ++i){
+		etasjeLys[i] = false
+	}
+	etasjeLys[lysIndex] = true
--- skeleton_project/source/lys.h
+++ skeleton_project/source/lys.h
@@ -1,2 +1,13 @@
 #pragma once
+#include <stdio.h>
+#include <stdbool.h>
 
+/*REDUNDANT:
+typedef enum {OPP, NED, PANEL} LysType;
+
+// antar at å sette et element i lyslisten til true også aktiverer lyset på boksen
+void aktiverKnappLys(LysType lysType, int lysIndex, bool On);
+*/
+
+
+void aktiverEtasjeLys(int lysIndex);
