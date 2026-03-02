40c20f5f352504ad01cff87e666ac5e73c171351
aslakmyhre <aslakmyh@stud.ntnu.no>
2026-03-02 10:17:05 +0100

    fix

--- skeleton_project/source/heisbevegelse.c
+++ skeleton_project/source/heisbevegelse.c
@@ -13,7 +13,7 @@ int getRetning() {
 
 void setRetning(int nyRetning) {
     printf("starter setRetning()\n");
-    if(retning != 0) {
+    if(nyRetning != 0) {
         retning = nyRetning;
     }
     elevio_motorDirection(nyRetning);
