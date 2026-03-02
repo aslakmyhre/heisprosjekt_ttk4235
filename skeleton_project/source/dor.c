44d31190e53739e85e3139e6bedf3ba1eec73354
Anders Sandvik <anders-01@live.no>
2026-03-02 11:33:05 +0100

    fiksa tidtaking

--- skeleton_project/source/dor.c
+++ skeleton_project/source/dor.c
@@ -16,23 +16,19 @@ void åpneDør(){
 void lukkDør(){
     printf("lukker dør\n");
     
-    int msekUtenObst = 0;
-    while (msekUtenObst <= 150) {
+    time_t stoppTid = time(NULL) + 3;
+
+    while (time(NULL) < stoppTid) {
         //nanosleep(&(struct timespec){0, 20*1000}, NULL);
 
         if (elevio_obstruction()) {
-            msekUtenObst = 0;
-        }
-        else {
-            msekUtenObst += 20;
-            printf("%d\n", msekUtenObst);
+            stoppTid = time(NULL) + 3;
         }
         // sjekk om hver knapp er trykket
         for(int f = 0; f < N_FLOORS; f++){
             for(int b = 0; b < N_BUTTONS; b++){
                 int btnPressed = elevio_callButton(f, b);
                 elevio_buttonLamp(f, b, btnPressed);
-                printf("aaa\n");
                 if(btnPressed){
                     opprettBestilling(f, b);
                 }
